5cc95a7f8330d47db8be3627b27a0e48e5e72d8e
Kashifalinawab <nawabkashifali@gmail.com>
2022-11-02 12:59:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header";
+import Content from "./Components/Content";
+import Total from "./Components/Total";
 
-function App() {
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Header course={course.name} />
+        <Content course={course.parts} />
+        <Total course={course.parts} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Content(props) {
+  //   const a = new Date().getFullYear();
+
+  return (
+    <div>
+      Content
+      {/* {a} */}
+    </div>
+  );
+}
+
+export default Content;
